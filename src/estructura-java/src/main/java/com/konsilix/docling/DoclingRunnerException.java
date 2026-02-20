c8400b5c67d0712bfa204595c55a04265cb41309
toribiodiego <toribio.diego23@gmail.com>
2026-02-19 20:18:11 -0500

    refactor: add konsilix package as renamed copy of estructura sources
    Duplicate the docling bridge classes from com.estructura to
    com.konsilix with copyright headers added. This prepares for
    the eventual migration away from the estructura package name.

---
+++ src/estructura-java/src/main/java/com/konsilix/docling/DoclingRunnerException.java
@@ -0,0 +1,15 @@
+/* (C) 2025 Konsilix. All rights reserved. */
+package com.konsilix.docling;
+
+/**
+ * Checked exception for wrapping Docling ingestion failures.
+ */
+public class DoclingRunnerException extends Exception {
+  public DoclingRunnerException(String message) {
+    super(message);
+  }
+
+  public DoclingRunnerException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
